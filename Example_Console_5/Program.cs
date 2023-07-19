d66949a05b992b480c925e0225d15a688203f00b
Art <artemgoryunov89@gmail.com>
2023-07-19 19:59:16 +0400

    Example Console 5

---
+++ Example_Console_5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+   Console.WriteLine("Специально для Маши"); 
+}
+else {
+
+    Console.Write("Привет!");
+    Console.WriteLine (username);
+
+}
